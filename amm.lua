6f34cbc9269cbca43e88b3d20b36bb92edde2d42
Marco Cardenas <mgtcardenas@gmail.com>
2021-02-11 11:24:12 -0600

    add the parameters and connect the knobs in macro page

--- amm.lua
+++ amm.lua
@@ -12,8 +12,17 @@ end
 
 defineParameter("pathB", nil, "untitledB", pathBChanged)
 
+defineParameter("silence_rate_percentage", nil, 0, 0.0, 1.0, 0.1) -- bipolar parameter from 0.0 to 1.0 with 0.1 steps
+defineParameter("lower_diff_percentage", nil, 0, 0.0, 1.0, 0.01) -- bipolar parameter from 0.0 to 1.0 with 0.01 steps
+defineParameter("upper_diff_percentage", nil, 0, 0.0, 1.0, 0.01) -- bipolar parameter from 0.0 to 1.0 with 0.01 steps
+defineParameter("num_attempts", nil, 0, 1, 5, 1) -- bipolar parameter from 0 to 5 with steps of 1
+
 function onNote(event)
-  runAsync(run, pathA, pathB)
+  print("Silence Rate: " .. silence_rate_percentage)
+  print("Lower Diff Percentage: " .. lower_diff_percentage)
+  print("Upper Diff Percentage: " .. upper_diff_percentage)
+  print("Num Attempts: " .. num_attempts)
+  --  runAsync(run, pathA, pathB)
 end
 
 function run(rutaA, rutaB)
