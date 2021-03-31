a12adcad45ed0057ef65f504f240d30688c4fff9
Marco Cardenas <mgtcardenas@gmail.com>
2021-03-31 16:18:18 -0600

    be able to process very long MIDI files

--- mix.lua
+++ mix.lua
@@ -1,39 +1,57 @@
 require('utils')
 
-
 function fill_z_vector(events, z_vector, ppq_position_limit)
   for i = 1, #events, 1 do
-    if events[i].type == EventType.noteOn then
-      for j = i + 1, #events, 1 do
-        if events[j].type == EventType.noteOff and events[j].ppqPosition <= ppq_position_limit and events[i].note == events[j].note then
-          -- 4.1.b. Fill with 1s the row from j1 to j2 (where j2 >= j1 and j1 & j2 are column indices) using the following parameters.
-          i1 = absolute_highest_note - events[i].note + 1 -- the row in the piano roll representation matrix
-          j1 = math.floor(events[i].ppqPosition * magnification_factor + 0.5) + 1 -- the start column in the piano roll representation matrix; + 0.5 is to make math.floor equivalent to math.round
-          j2 = math.floor(events[j].ppqPosition * magnification_factor + 0.5) + 1 -- the end column in the piano roll representation matrix; + 0.5 is to make math.floor equivalent to math.round
-          xi = (i1 - 1) * n + (j1 - 1) + 1
-          xj = (i1 - 1) * n + (j2 - 1) + 1
-          -- print(i1, j1, j2, events[i], events[j], xi, xj)
-          for z = xi, xj, 1 do
-            z_vector[z] = 1
-          end -- for - z
-          break -- just find the closest noteOff event
-        end -- if
-      end -- for - j
-    elseif events[i].type == EventType.noteOff and events[i].ppqPosition > ppq_position_limit then
-      print(events[i])
-      break -- just get out of the function, you're done
-    end -- if
+    if events[i].ppqPosition < ppq_position_limit then
+      if events[i].type == EventType.noteOn then
+        for j = i + 1, #events, 1 do
+          if events[j].type == EventType.noteOff and events[i].note == events[j].note and events[j].ppqPosition <= ppq_position_limit then
+            -- 4.1.b. Fill with 1s the row from j1 to j2 (where j2 >= j1 and j1 & j2 are column indices) using the following parameters.
+            i1 = absolute_highest_note - events[i].note + 1 -- the row in the piano roll representation matrix
+            j1 = math.floor(events[i].ppqPosition * magnification_factor + 0.5) + 1 -- the start column in the piano roll representation matrix; + 0.5 is to make math.floor equivalent to math.round
+            j2 = math.floor(events[j].ppqPosition * magnification_factor + 0.5) + 1 -- the end column in the piano roll representation matrix; + 0.5 is to make math.floor equivalent to math.round
+            xi = (i1 - 1) * n + (j1 - 1) + 1
+            xj = (i1 - 1) * n + (j2 - 1) + 1
+            -- print(i1, j1, j2, events[i], events[j], xi, xj)
+            for z = xi, xj, 1 do
+              z_vector[z] = 1
+            end -- for - z
+            break -- just find the first closest noteOff event within the ppq_position_limit
+          elseif events[j].type == EventType.noteOff and events[i].note == events[j].note and events[j].ppqPosition > ppq_position_limit then
+            break -- The noteOff event of the current note (i) is beyond the ppq_position_limit
+          end -- if noteOff
+        end -- for - j
+      end -- if noteOn
+    else
+      break -- we are looking at the notes which begin after the ppq_position_limit, we are done!
+    end -- if before ppq_position_limit
   end -- for - i
-end
+end -- fill_z_vector
 
-
-function mix(midi_sequence_a, midi_sequenc_b, result_path, silence_rate, lower_diff_percentage, upper_diff_percentage, num_attempts, ppq_position_limit)
+function mix(midi_sequence_a, midi_sequence_b, result_path, silence_rate, lower_diff_percentage, upper_diff_percentage, num_attempts, ppq_position_limit)
   -- 0. ESTABLISH CONSTANTS
   magnification_factor = 1
   p = 2 -- p is the number of patterns
 
+  --[[ 1. PREPROCESS MIDI FILE A -- ]]
+  -- 1.1 Find the track that has events in MIDI A
+  midi_a_track_num = 1
+  for i = 1, #midi_sequence_a.tracks, 1 do
+    if (#midi_sequence_a.tracks[i].events > 0) then
+      midi_a_track_num = i
+      break
+    end
+  end
+
+  if (#midi_sequence_a.tracks[midi_a_track_num].events == 0) then
+    messageBox("MIDI A has no tracks with events in it")
+    return
+  end
+
+  --print("MIDI A Track Num: " .. midi_a_track_num)
+
   -- 1.2 Sort its events (to ensure that the following steps will work correctly)
-  sortEvents(midi_sequence_a.tracks[1].events)
+  sortEvents(midi_sequence_a.tracks[midi_a_track_num].events)
 
   --[[ 1.3 Loop through the NoteOffEvents and keep track of …
     a. The lowest note
@@ -44,26 +62,46 @@ function mix(midi_sequence_a, midi_sequenc_b, result_path, silence_rate, lower_d
   midi_a_highest_note = -math.huge
   midi_a_largest_ppqposition = -math.huge -- math.huge = infinite. It is possible to do -math.huge to get minus infinite
 
-  for i, event in ipairs(midi_sequence_a.tracks[1].events) do
+  for i, event in ipairs(midi_sequence_a.tracks[midi_a_track_num].events) do
     if event.type == EventType.noteOff and event.ppqPosition <= ppq_position_limit then
-      if event.note < midi_a_lowest_note then midi_a_lowest_note = event.note end
-      if event.note > midi_a_highest_note then midi_a_highest_note = event.note end
-      if event.ppqPosition > midi_a_largest_ppqposition then midi_a_largest_ppqposition = event.ppqPosition end
+      if event.note < midi_a_lowest_note then
+        midi_a_lowest_note = event.note
+      end
+      if event.note > midi_a_highest_note then
+        midi_a_highest_note = event.note
+      end
+      if event.ppqPosition > midi_a_largest_ppqposition then
+        midi_a_largest_ppqposition = event.ppqPosition
+      end
       -- print(i, event)
     elseif event.type == EventType.noteOff and event.ppqPosition > ppq_position_limit then
       break
     end -- if
   end -- for
 
-  --  print("The lowest note in midi a is: " .. midi_a_lowest_note)
-  --  print("The highest note in midi a is: " .. midi_a_highest_note)
-  --  print("The largest ppqposition in midi a is: " .. midi_a_largest_ppqposition)
+  --print("The lowest note in midi a is: " .. midi_a_lowest_note)
+  --print("The highest note in midi a is: " .. midi_a_highest_note)
+  --print("The largest ppqposition in midi a is: " .. midi_a_largest_ppqposition)
 
   --[[ 2. PREPROCESS MIDI FILE B -- ]]
-  -- 2.1 Read the MIDI file B (obtain the array of events)
+  -- 2.1 Find the track that has events in MIDI B
+  midi_b_track_num = 1
+  for i = 1, #midi_sequence_b.tracks, 1 do
+    if (#midi_sequence_b.tracks[i].events > 0) then
+      midi_b_track_num = i
+      break
+    end
+  end
+
+  if (#midi_sequence_b.tracks[midi_b_track_num].events == 0) then
+    messageBox("MIDI B has no tracks with events in it")
+    return
+  end
+
+  --print("MIDI B Track Num: " .. midi_b_track_num)
 
   -- 2.2 Sort its events (to ensure that the following steps will work correctly)
-  sortEvents(midi_sequenc_b.tracks[1].events)
+  sortEvents(midi_sequence_b.tracks[midi_b_track_num].events)
 
   --[[ 2.3 Loop through the NoteOffEvents and keep track of …
     a. The lowest note
@@ -74,20 +112,26 @@ function mix(midi_sequence_a, midi_sequenc_b, result_path, silence_rate, lower_d
   midi_b_highest_note = -math.huge
   midi_b_largest_ppqposition = -math.huge -- math.huge = infinite. It is possible to do -math.huge to get minus infinite
 
-  for i, event in ipairs(midi_sequenc_b.tracks[1].events) do
+  for i, event in ipairs(midi_sequence_b.tracks[midi_b_track_num].events) do
     if event.type == EventType.noteOff and event.ppqPosition <= ppq_position_limit then
-      if event.note < midi_b_lowest_note then midi_b_lowest_note = event.note end
-      if event.note > midi_b_highest_note then midi_b_highest_note = event.note end
-      if event.ppqPosition > midi_b_largest_ppqposition then midi_b_largest_ppqposition = event.ppqPosition end
+      if event.note < midi_b_lowest_note then
+        midi_b_lowest_note = event.note
+      end
+      if event.note > midi_b_highest_note then
+        midi_b_highest_note = event.note
+      end
+      if event.ppqPosition > midi_b_largest_ppqposition then
+        midi_b_largest_ppqposition = event.ppqPosition
+      end
       -- print(i, event)
     elseif event.type == EventType.noteOff and event.ppqPosition > ppq_position_limit then
       break
     end -- if
   end -- for
 
-  --  print("The lowest note in midi b is: " .. midi_b_lowest_note)
-  --  print("The highest note in midi b is: " .. midi_b_highest_note)
-  --  print("The largest ppqposition in midi b is: " .. midi_b_largest_ppqposition)
+  --print("The lowest note in midi b is: " .. midi_b_lowest_note)
+  --print("The highest note in midi b is: " .. midi_b_highest_note)
+  --print("The largest ppqposition in midi b is: " .. midi_b_largest_ppqposition)
 
   --[[ 3. PREPARE for Z VECTORS -- ]]
   -- 3.1 Obtain the following variables
@@ -95,16 +139,16 @@ function mix(midi_sequence_a, midi_sequenc_b, result_path, silence_rate, lower_d
   absolute_highest_note = math.max(midi_a_highest_note, midi_b_highest_note)
   absolute_largest_ppqposition = math.max(midi_a_largest_ppqposition, midi_a_largest_ppqposition)
 
-  --  print("The absolute lowest note is: " .. absolute_lowest_note)
-  --  print("The absolute highest note is: " .. absolute_highest_note)
-  --  print("The absolute largest ppqposition is: " .. absolute_largest_ppqposition)
+  --print("The absolute lowest note is: " .. absolute_lowest_note)
+  --print("The absolute highest note is: " .. absolute_highest_note)
+  --print("The absolute largest ppqposition is: " .. absolute_largest_ppqposition)
 
 
   --3.2 Create a table (array) full of zeroes (or -1s) with size (m x n) for each of the MIDI files…
   m = absolute_highest_note - absolute_lowest_note + 1 -- + 1 because 1-based Lua tables
   n = absolute_largest_ppqposition * magnification_factor + 1 -- + 1 in order to avoid incorrectly filling the z vectors (training vectors)
-  --  print("m = " .. m)
-  --  print("n = " .. n)
+  --print("m = " .. m)
+  --print("n = " .. n)
 
   -- 3.3 Establish the limits for the acceptable number of notes
   lower_diff_limit = math.floor((m * n) * lower_diff_percentage)
@@ -119,14 +163,14 @@ function mix(midi_sequence_a, midi_sequenc_b, result_path, silence_rate, lower_d
 
   -- 4.1 Iterate through all the NoteOnEvents.
   -- 4.1.a. For every NoteOnEvent, search starting from the index of the just found NoteOnEvent looking for the closest NoteOffEvent that corresponds to the same MIDI Note Value.
-  eventsA = midi_sequence_a.tracks[1].events
+  eventsA = midi_sequence_a.tracks[midi_a_track_num].events
   fill_z_vector(eventsA, za, ppq_position_limit)
 
   --[[ 5. FILL ZB (VECTOR OF NOTES B) -- ]]
   zb = get_table_full_of_minus_ones(m * n)
   -- 5.1 Iterate through all the NoteOnEvents.
   -- 5.1.a. For every NoteOnEvent, search starting from the index of the just found NoteOnEvent looking for the closest NoteOffEvent that corresponds to the same MIDI Note Value.
-  eventsB = midi_sequenc_b.tracks[1].events
+  eventsB = midi_sequence_b.tracks[midi_b_track_num].events
   fill_z_vector(eventsB, zb, ppq_position_limit)
 
   --[[ 6. OBTAIN ii --]]
